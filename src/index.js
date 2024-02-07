48ca633f0bd90aaec0869f3890c3f581e85684fc
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2024-02-07 21:40:16 +0300

    ready

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createRoot } from "react-dom/client";
+
+import Sayac from './components/Sayac';
+import Input from './components/Input';
+import RuhHalleri from './components/RuhHalleri';
+import Dondurucu from './components/Dondurucu';
+import Kareler from './components/Kareler';
+import Programcilar from './components/Programcilar';
+
+
+const container = document.getElementById("root");
+const root = createRoot(container)
+root.render(<>
+  
+  <Input />
+  <Sayac />
+
+  <Input />
+  <Sayac />
+
+  <Input />
+  <Sayac />
+  {/* 
+  <Sayac />
+  <RuhHalleri />
+  <Dondurucu />
+  <Input />
+  <Kareler />
+  <Programcilar /> */}
+</>);
